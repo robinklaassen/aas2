aa2d1e5eff3c50720ab9f41ecff80ef0bd376ddb
Vincent Hagen <vinnie@script4web.nl>
2019-04-21 10:50:47 +0200

    added migrations for roles

---
+++ database/migrations/2019_04_21_104846_create_user_roles_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_roles', function (Blueprint $table) {
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_roles', function (Blueprint $table) {
+            //
+        });
+    }
+}
