276775b4ed089ccfb19c3535ab2043452dda5873
blackshadev <vinnie@script4web.nl>
2019-04-22 13:15:14 +0200

    seeding works

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -17,31 +17,33 @@ class UserTableSeeder extends Seeder {
 	public function run()
 	{
 		DB::table('users')->delete();
-		
+
 		$member = Member::find(1);
 		$user = new User;
 		$user->username = 'ranonkeltje';
 		$user->password = bcrypt('ranonkeltje');
 		$user->is_admin = 2;
+		$user->save();
 		$user->roles()->attach(Role::find(1));
 		$member->user()->save($user);
-		
-		$role = Role::find(3);
+
 		$member = Member::find(2);
 		$user = new User;
 		$user->username = 'jon';
 		$user->password = bcrypt('snow');
 		$user->is_admin = 0;
+		$user->save();
 		$user->roles()->attach(Role::find(2));
+		$user->roles()->attach(Role::find(3));
 		$member->user()->save($user);
-		
+
 		$participant = Participant::find(2);
 		$user = new User;
 		$user->username = 'annabelle';
 		$user->password = bcrypt('zomers');
 		$user->is_admin = 0;
 		$participant->user()->save($user);
-		
+
 	}
 
 }
