46f452388d59d463d25a24fc6787aa27fff6ce04
Robin Klaassen <13085028+robinklaassen@users.noreply.github.com>
2019-05-18 13:28:21 +0200

    Explicit clear compilation and force migrate

--- deploy_prd.sh
+++ deploy_prd.sh
@@ -7,7 +7,8 @@ php artisan down
 git pull origin master
 php composer.phar self-update
 php composer.phar update
-php artisan migrate
+php artisan clear-compiled
+php artisan migrate --force
 php artisan optimize
 php artisan up
 
