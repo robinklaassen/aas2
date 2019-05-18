e130280e34f9ee87d26258bca36cb85e65079a0a
Robin Klaassen <13085028+robinklaassen@users.noreply.github.com>
2019-05-18 13:45:57 +0200

    removed force-https package

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -11,7 +11,6 @@ class Kernel extends HttpKernel {
 	 */
 	protected $middleware = [
 		\App\Http\Middleware\CheckForMaintenanceMode::class,
-		\Shin1x1\ForceHttpsUrlScheme\ForceHttpsUrlScheme::class, // <---added
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
