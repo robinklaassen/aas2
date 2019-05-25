1ce9608ff46eaf43d59961780ea902b7c236d386
blackshadev <vinnie@script4web.nl>
2019-05-25 09:57:30 +0200

    finished first test

--- tests/Feature/RegistationTest.php
+++ tests/Feature/RegistationTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature;
 
 use Mockery;
 use Tests\TestCase;
+use App\Event;
+use App\Participant;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -11,6 +13,20 @@ use App\Helpers\Payment\MolliePaymentProvider;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\DB;
+
+function clearDB()
+{
+    DB::statement("
+    delete from users
+     where profile_type = 'App\Participant'
+       and profile_id in (select p.id from participants p where p.achternaam = 'Test' )
+    ");
+    DB::statement("
+    delete from participants
+     where achternaam = 'Test'
+
+    ");
+}
 
 class RegistationTest extends TestCase
 {
@@ -26,18 +42,18 @@ class RegistationTest extends TestCase
         "telefoon_deelnemer" => "0612345678",
         "email_deelnemer" => "keesje@test.com",
         "adres" => "TestLaan",
-        "postcode" => "1111AA",
+        "postcode" => "1111 AA",
         "plaats" => "Zwapperdam",
         "telefoon_ouder_vast" => "0361234567",
         "telefoon_ouder_mobiel" => "0612345678",
-        "email_ouder" => "keesje@test.com",
-        "mag_gemaild" => "0",
-        "selected_camp" => "5",
-        "inkomen" => "0",
+        "email_ouder" => "kees@test.com",
+        "mag_gemaild" => 0,
+        "selected_camp" => 5,
+        "inkomen" => 0,
         "school" => "HierLeerJeNietsSchool",
         "niveau" => "VMBO",
-        "klas" => "2",
-        "vak0" => "3",
+        "klas" => 2,
+        "vak0" => 3,
         "vakinfo0" => "zoiets",
         "vak1" => 3,
         "vakinfo1" => "Daar snap ik toch geen kut van joh, iets met E=MC2",
@@ -49,17 +65,26 @@ class RegistationTest extends TestCase
         "vakinfo4" => "",
         "vak5" => 0,
         "vakinfo5" => "",
-        "iDeal" => "1",
+        "iDeal" => 1,
         "hoebij" => ["Nieuwsbrief school"],
         "hoebij_anders" => "",
         "opmerkingen" => "",
-        "voorwaarden" => "1",
-        "privacy" => "1"
+        "voorwaarden" => 1,
+        "privacy" => 1
     ];
+    private $event;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        clearDB();
+        $this->event = Event::findOrFail($this->data["selected_camp"]);
+    }
 
     protected function tearDown(): void
     {
-        // DB::statement('delete from users u where profile_type = '' ");
+        clearDB();
+        parent::tearDown();
     }
 
     // public function testParticipantRegistrationWithIDeal()
@@ -79,15 +104,14 @@ class RegistationTest extends TestCase
         Mail::fake();
 
         $this->instance(MolliePaymentProvider::class, Mockery::mock(MolliePaymentProvider::class, function ($mock) {
-            $mock->shouldNotReceive('process')->once();
+            $mock->shouldNotReceive('process');
         }));
 
         $this->data["iDeal"] = "0";
 
         $response = $this->post('/register-participant', $this->data);
 
-        $response->assertStatus(200);
-
+        // check db
         $username = strtolower(substr($this->data["voornaam"], 0, 1) . $this->data["achternaam"]);
         $this->assertDatabaseHas('users', [
             "username" => $username,
@@ -100,7 +124,7 @@ class RegistationTest extends TestCase
             "achternaam" => $this->data["achternaam"],
             "geboortedatum" => $this->data["geboortedatum"],
             "geslacht" => $this->data["geslacht"],
-            "adres" => $this->data["geslacht"],
+            "adres" => $this->data["adres"],
             "postcode" => $this->data["postcode"],
             "plaats" => $this->data["plaats"],
             "telefoon_deelnemer" => $this->data["telefoon_deelnemer"],
@@ -115,5 +139,12 @@ class RegistationTest extends TestCase
             "klas" => $this->data["klas"],
             "inkomensverklaring" => null
         ]);
+
+        // Check output
+        $response->assertStatus(200);
+        $response->assertSee("€ " . $this->event->prijs);
+        $response->assertSee("NL68 TRIO 0198 4197 83");
+        $response->assertSee("naam deelnemer + deze kampcode: " . $this->event->code);
+        $response->assertSee(Participant::INCOME_DESCRIPTION_TABLE[$this->data["inkomen"]]);
     }
 }
