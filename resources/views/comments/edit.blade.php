b968b3ae38017c90e288b0e472bb904ba2503f59
blackshadev <vinnie@script4web.nl>
2019-06-22 16:24:33 +0200

    Working on comment view

---
+++ resources/views/comments/edit.blade.php
@@ -0,0 +1,22 @@
+@extends('master')
+
+@section('title')
+Opmerking bewerken
+@endsection
+
+@section('content')
+
+<h1>Opmerking bewerken</h1>
+<h4>voor ...</h4>
+
+<hr />
+
+@include ('errors.list')
+
+{!! Form::open(['url' => 'comments/'.$comment->id ], 'method' => 'PATCH']) !!}
+
+@include ('comments.form')
+
+{!! Form::close() !!}
+
+@endsection
