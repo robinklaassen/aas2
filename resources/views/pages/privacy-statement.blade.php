13c9e4c600543faed7bae09fb3ee2189c89d8c6f
blackshadev <vinnie@script4web.nl>
2019-08-06 20:40:15 +0200

    Workign on form

--- resources/views/pages/privacy-statement.blade.php
+++ resources/views/pages/privacy-statement.blade.php
@@ -7,7 +7,7 @@ Privacy statement
 @section('content')
 
 <div class="panel panel-default">
-    <div class="panel-heading">
+    <div class="panel-heading" style="height:62px">
         {{ Form::model(Auth::user(), ['METHOD' => 'POST', 'url' => 'privacy']) }}
         <label>
 
@@ -15,7 +15,7 @@ Privacy statement
             Ik geef Anderwijs toestemming om mijn gegevens te verwerken zoals beschreven in het onderstaande privacystatement
 
         </label>
-        {{ Form::submit('Opslaan', [ "class" => "button" ]) }}
+        {{ Form::submit('Opslaan', [ "class" => "btn btn-primary pull-right" ]) }}
         {{ Form::close() }}
     </div>
     <div class="panel-body">
