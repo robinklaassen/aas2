8788c0eadec53ade417c71264092d5edfaf33a45
Vincent Hagen <vinnie@script4web.nl>
2019-08-24 11:40:02 +0200

    filled privacy field in migration

--- database/migrations/2019_08_05_143300_add_privacy_to_users_table.php
+++ database/migrations/2019_08_05_143300_add_privacy_to_users_table.php
@@ -16,6 +16,9 @@ class AddPrivacyToUsersTable extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->timestamp('privacy')->nullable();
         });
+        DB::Statement("
+            UPDATE users SET privacy = created_at where created_at >= '2018-06-01'
+        ");
     }
 
     /**
