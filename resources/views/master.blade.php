1cea44c426634cd1461c7376293495ffb9149e39
Robin Klaassen <13085028+robinklaassen@users.noreply.github.com>
2019-08-26 07:30:00 +0200

    extracted info modal to separate file

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -50,28 +50,9 @@
 	</div>
 	
 	<!-- Info modal -->
-	<div class="modal fade" id="infoModal" tabindex="-1" role="dialog">
-	  <div class="modal-dialog" role="document">
-		<div class="modal-content">
-		  <div class="modal-header">
-			<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-			<h4 class="modal-title">Over AAS 2.0</h4>
-		  </div>
-		  <div class="modal-body">
-			<p>AAS 2.0 is ontwikkeld door <a href="mailto:robin@anderwijs.nl">Robin Klaassen</a> in april-mei 2015. Het systeem draait op het PHP framework <a href="http://laravel.com/">Laravel</a> en de voorkant is gebaseerd op <a href="http://getbootstrap.com/">Bootstrap 3</a>, met de <a href="https://bootswatch.com/cosmo/">Cosmo</a> template. Interactieve tabellen worden mogelijk gemaakt door <a href="https://datatables.net">DataTables</a>, kaarten en grafieken door Google's <a href="https://developers.google.com/maps/">Maps</a> en <a href="https://developers.google.com/chart/">Charts</a>.</p>
+	@include('partials.info')
 
-			<p>AAS staat voor Anderwijs Administratie Systeem en is voor het eerst digitaal ontwikkeld door Dave Liefbroer in 2008.</p>
-
-			<p>Bug reports, feature requests en/of vragen graag per mail naar <a href="mailto:webmaster@anderwijs.nl">webmaster@anderwijs.nl</a>. Wees hierbij zo specifiek mogelijk.</p>
-		  </div>
-		  <div class="modal-footer">
-			<button type="button" class="btn btn-primary" data-dismiss="modal">Bedankt!</button>
-		  </div>
-		</div><!-- /.modal-content -->
-	  </div><!-- /.modal-dialog -->
-	</div><!-- /.modal -->
-
-	<!-- Load jQuery and Boostrap scripts -->
+	<!-- Load jQuery and Bootstrap scripts -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 	
@@ -119,7 +100,7 @@
 			}
 		} );
 		
-		// Fix DataTables in Boostrap tabs
+		// Fix DataTables in Bootstrap tabs
 		$("a[data-toggle=\"tab\"]").on("shown.bs.tab", function (e) {
 		  console.log( 'show tab' );
 			$($.fn.dataTable.tables(true)).DataTable()
---
+++ resources/views/partials/info.blade.php
@@ -0,0 +1,21 @@
+<!-- Info modal -->
+<div class="modal fade" id="infoModal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <h4 class="modal-title">Over AAS 2.0</h4>
+        </div>
+        <div class="modal-body">
+          <p>AAS 2.0 is ontwikkeld door <a href="mailto:robin@anderwijs.nl">Robin Klaassen</a> in april-mei 2015. Het systeem draait op het PHP framework <a href="http://laravel.com/">Laravel</a> en de voorkant is gebaseerd op <a href="http://getbootstrap.com/">Bootstrap 3</a>, met de <a href="https://bootswatch.com/cosmo/">Cosmo</a> template. Interactieve tabellen worden mogelijk gemaakt door <a href="https://datatables.net">DataTables</a>, kaarten en grafieken door Google's <a href="https://developers.google.com/maps/">Maps</a> en <a href="https://developers.google.com/chart/">Charts</a>.</p>
+
+          <p>AAS staat voor Anderwijs Administratie Systeem en is voor het eerst digitaal ontwikkeld door Dave Liefbroer in 2008.</p>
+
+          <p>Bug reports, feature requests en/of vragen graag per mail naar <a href="mailto:webmaster@anderwijs.nl">webmaster@anderwijs.nl</a>. Wees hierbij zo specifiek mogelijk.</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary" data-dismiss="modal">Bedankt!</button>
+        </div>
+      </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+  </div><!-- /.modal -->
