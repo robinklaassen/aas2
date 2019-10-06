384e3a3c61b1b2e7dd4457be5881e1313e341893
Robin Klaassen <13085028+robinklaassen@users.noreply.github.com>
2019-10-06 19:53:15 +0200

    fixed problems in master js

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -13,7 +13,8 @@
 
 	<!-- Load Bootstrap extension for DataTables -->
 	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.10/css/dataTables.bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.0.0/css/responsive.bootstrap.min.css">
+	<link rel="stylesheet" type="text/css"
+		href="https://cdn.datatables.net/responsive/2.0.0/css/responsive.bootstrap.min.css">
 	{{ Html::style('css/custom.css') }}
 
 	<!-- Proper text alignment in multiline tooltips -->
@@ -42,11 +43,15 @@
 		@yield('content')
 
 		<!-- Footer -->
-		<div style="display: inline-flex; float: right; flex-wrap: wrap; border-top: 1px solid #ddd; padding-top: 10px; margin-top: 10px;">
+		<div
+			style="display: inline-flex; float: right; flex-wrap: wrap; border-top: 1px solid #ddd; padding-top: 10px; margin-top: 10px;">
 			<span class="btn btn-sm" disabled="true">AAS 2.0 is het Anderwijs Administratiesysteem.</span>
-			<button type="button" class="btn btn-link btn-sm" data-toggle="modal" data-target="#infoModal">Meer informatie</button>
-			<button type="button" class="btn btn-link btn-sm"><a href="{{ url('privacy') }}">Privacystatement</a></button>
-			<button type="button" class="btn btn-link btn-sm"><a href="mailto:webmaster@anderwijs.nl">Mail de webmaster</a></button>
+			<button type="button" class="btn btn-link btn-sm" data-toggle="modal" data-target="#infoModal">Meer
+				informatie</button>
+			<button type="button" class="btn btn-link btn-sm"><a
+					href="{{ url('privacy') }}">Privacystatement</a></button>
+			<button type="button" class="btn btn-link btn-sm"><a href="mailto:webmaster@anderwijs.nl">Mail de
+					webmaster</a></button>
 		</div>
 	</div>
 
@@ -58,14 +63,20 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 
 	<!-- Load DataTables scripts -->
-	<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.10/js/jquery.dataTables.min.js"></script>
-	<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.10/js/dataTables.bootstrap.min.js"></script>
-	<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/responsive/2.0.0/js/dataTables.responsive.min.js"></script>
-	<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/responsive/2.0.0/js/responsive.bootstrap.min.js"></script>
+	<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.10/js/jquery.dataTables.min.js">
+	</script>
+	<script type="text/javascript" charset="utf8"
+		src="https://cdn.datatables.net/1.10.10/js/dataTables.bootstrap.min.js"></script>
+	<script type="text/javascript" charset="utf8"
+		src="https://cdn.datatables.net/responsive/2.0.0/js/dataTables.responsive.min.js"></script>
+	<script type="text/javascript" charset="utf8"
+		src="https://cdn.datatables.net/responsive/2.0.0/js/responsive.bootstrap.min.js"></script>
 
 	<!-- Load Modernizr and webshims scripts -->
-	<script type="text/javascript" charset="utf8" src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
-	<script type="text/javascript" charset="utf8" src="//cdnjs.cloudflare.com/ajax/libs/webshim/1.15.7/minified/polyfiller.js"></script>
+	<script type="text/javascript" charset="utf8"
+		src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+	<script type="text/javascript" charset="utf8"
+		src="//cdnjs.cloudflare.com/ajax/libs/webshim/1.15.7/minified/polyfiller.js"></script>
 
 	<script type="text/javascript">
 		// Set date functionality to HTML 5 in unsupported browsers
@@ -74,7 +85,7 @@
 		}
 
 		// On DOM ready...
-		($(document).ready(function() {
+		$(document).ready(function() {
 			// Removes unimportant alerts after 3 seconds
 			$('div.alert').not('.alert-important').delay(3000).slideUp(300);
 
@@ -83,33 +94,32 @@
 
 			// Set DataTables default language things
 			$.extend($.fn.dataTable.defaults, {
-					"responsive": true,
-					"language": {
-						"emptyTable": "Geen beschikbare data",
-						"lengthMenu": "Laat  _MENU_  rijen zien",
-						"zeroRecords": "Geen rijen gevonden!",
-						"info": "Rij _START_ t/m _END_ van _TOTAL_",
-						"infoEmpty": "Geen beschikbare rijen",
-						"infoFiltered": "(gefilterd uit _MAX_ rijen totaal)",
-						"search": "Zoeken:",
-						"paginate": {
-							"first": "Eerste",
-							"last": "Laatste",
-							"next": "Volgende",
-							"previous": "Vorige"
-						}
+				"responsive": true,
+				"language": {
+					"emptyTable": "Geen beschikbare data",
+					"lengthMenu": "Laat  _MENU_  rijen zien",
+					"zeroRecords": "Geen rijen gevonden!",
+					"info": "Rij _START_ t/m _END_ van _TOTAL_",
+					"infoEmpty": "Geen beschikbare rijen",
+					"infoFiltered": "(gefilterd uit _MAX_ rijen totaal)",
+					"search": "Zoeken:",
+					"paginate": {
+						"first": "Eerste",
+						"last": "Laatste",
+						"next": "Volgende",
+						"previous": "Vorige"
 					}
 				}
 			});
 
-		// Fix DataTables in Bootstrap tabs
-		$("a[data-toggle=\"tab\"]").on("shown.bs.tab", function(e) {
-			console.log('show tab');
-			$($.fn.dataTable.tables(true)).DataTable()
-				.columns.adjust()
-				.responsive.recalc();
+			// Fix DataTables in Bootstrap tabs
+			$("a[data-toggle=\"tab\"]").on("shown.bs.tab", function(e) {
+				console.log('show tab');
+				$($.fn.dataTable.tables(true)).DataTable()
+					.columns.adjust()
+					.responsive.recalc();
+			});
 		});
-		}));
 	</script>
 
 	<!-- Footer content (page-specific scripts etc.) -->
