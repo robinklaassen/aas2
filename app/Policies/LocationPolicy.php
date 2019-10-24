0d8bd4542d11ba7fe413c9236b3d6040fec9fb03
Vincent Hagen <vinnie@script4web.nl>
2019-10-24 10:26:00 +0200

    more rights

---
+++ app/Policies/LocationPolicy.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Policies;
+
+use App\Location;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class LocationPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any locations.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        return $user->hasCapability("locations::info::basic");
+    }
+
+    /**
+     * Determine whether the user can view the location.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Location  $location
+     * @return mixed
+     */
+    public function view(User $user, Location $location)
+    {
+        return $user->hasCapability("locations::info::basic");
+    }
+
+    public function viewAdvanced(User $user, Location $location)
+    {
+        return $user->hasCapability("locations::info::advanced");
+    }
+
+    /**
+     * Determine whether the user can create locations.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        return $user->hasCapability("locations::create");
+    }
+
+    /**
+     * Determine whether the user can update the location.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Location  $location
+     * @return mixed
+     */
+    public function update(User $user, Location $location)
+    {
+        return $user->hasCapability("locations::edit::basic");
+    }
+
+    /**
+     * Determine whether the user can delete the location.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Location  $location
+     * @return mixed
+     */
+    public function delete(User $user, Location $location)
+    {
+        return $user->hasCapability("locations::delete");
+    }
+
+    /**
+     * Determine whether the user can restore the location.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Location  $location
+     * @return mixed
+     */
+    public function restore(User $user, Location $location)
+    {
+        return $user->hasCapability("locations::create");
+    }
+
+    /**
+     * Determine whether the user can permanently delete the location.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Location  $location
+     * @return mixed
+     */
+    public function forceDelete(User $user, Location $location)
+    {
+        return $user->hasCapability("locations::delete");
+    }
+}
