832aa0c3e350eb3d2d9fbbf04f3db67ef552bc4e
Robin Klaassen <13085028+robinklaassen@users.noreply.github.com>
2020-02-28 15:19:11 +0100

    small refactor

--- database/migrations/2020_02_28_145004_add_roles_to_existing_users.php
+++ database/migrations/2020_02_28_145004_add_roles_to_existing_users.php
@@ -16,23 +16,19 @@ class AddRolesToExistingUsers extends Migration
         $part_role = Role::where("tag", "=", "participant")->firstOrFail();
         $memb_role = Role::where("tag", "=", "member")->firstOrFail();
 
-
-        DB::statement('
+        $query = '
             insert into user_role (user_id , role_id, created_by, created_at)
             select u.id, :role, 0, NOW()
-              from users u
-             where u.profile_type = :type
-        ', [
+            from users u
+            where u.profile_type = :type
+        '
+
+        DB::statement($query, [
             "type" => "App\\Participant",
             "role" => $part_role->id
         ]);
 
-        DB::statement('
-            insert into user_role (user_id , role_id, created_by, created_at)
-            select u.id, :role, 0, NOW()
-              from users u
-             where u.profile_type = :type 
-        ', [
+        DB::statement($query, [
             "type" => "App\\Member",
             "role" => $memb_role->id
         ]);
