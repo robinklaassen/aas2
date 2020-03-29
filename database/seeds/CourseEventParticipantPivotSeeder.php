ef9b5a2bc6fd401f6a8d7537a946988f2b63db1b
Robin Klaassen <rklaassen@infiniot.nl>
2020-03-29 13:45:40 +0200

    added package to participantstable in online event

--- database/seeds/CourseEventParticipantPivotSeeder.php
+++ database/seeds/CourseEventParticipantPivotSeeder.php
@@ -21,7 +21,8 @@ class CourseEventParticipantPivotSeeder extends Seeder
 			['course_id' => 2, 'event_id' => 1, 'participant_id' => 1, 'info' => 'Ai kent spiek inglies ferrie wel'],
 			['course_id' => 4, 'event_id' => 1, 'participant_id' => 1, 'info' => 'Hier staat echt een enorm verhaal, je weet wel, als een ouder echt een epistel meestuurt over de leerproblemen van zijn/haar zoon of dochter.'],
 			['course_id' => 3, 'event_id' => 1, 'participant_id' => 2, 'info' => 'Bij het volgende vak staat niets'],
-			['course_id' => 4, 'event_id' => 1, 'participant_id' => 2, 'info' => null]
+			['course_id' => 4, 'event_id' => 1, 'participant_id' => 2, 'info' => null],
+			['course_id' => 3, 'event_id' => 7, 'participant_id' => 3, 'info' => 'Wauw, online bijles, wat vet']
 		]);
 	}
 }
