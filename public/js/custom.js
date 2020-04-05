45349e290591819591111b75294f69359a2afc8c
Vincent Hagen <vinnie@script4web.nl>
2020-04-05 11:25:29 +0200

    split emails in event.email view

--- public/js/custom.js
+++ public/js/custom.js
@@ -3,7 +3,7 @@ function makeTableSortable(selector) {
 
   function getColumnOptions() {
     let columnOptions = [];
-    jTable.find("thead th").each(function(i) {
+    jTable.find("thead th").each(function (i) {
       let option =
         $(this).data("orderable") !== undefined ? null : { orderable: false };
       columnOptions.push(option);
@@ -14,6 +14,27 @@ function makeTableSortable(selector) {
   jTable.DataTable({
     paging: false,
     order: [[0, "asc"]],
-    columns: getColumnOptions()
+    columns: getColumnOptions(),
   });
+}
+
+function copyToClipboard(clickElement, idCopyElement) {
+  var copyEl = document.querySelector(idCopyElement);
+  var range = document.createRange();
+  range.selectNodeContents(copyEl);
+  var sel = window.getSelection();
+  sel.removeAllRanges();
+  sel.addRange(range);
+
+  document.execCommand("copy");
+
+  $(clickElement).tooltip({
+    title: "Gekopieerd",
+    trigger: "manual",
+  });
+  $(clickElement).tooltip("show");
+
+  setTimeout(function () {
+    $(clickElement).tooltip("hide");
+  }, 3000);
 }
