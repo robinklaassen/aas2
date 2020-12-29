ade55b185fef8248a0858e1110d115157ef21f5e
Robin Klaassen <rklaassen@infiniot.nl>
2020-12-29 08:43:47 +0100

    restored js code for sortable datatables

---
+++ resources/js/helpers/tableSortable.js
@@ -0,0 +1,21 @@
+function makeTableSortable(selector) {
+    var jTable = $(selector);
+
+    function getColumnOptions() {
+        let columnOptions = [];
+        jTable.find("thead th").each(function (i) {
+            let option =
+                $(this).data("orderable") !== undefined ? null : { orderable: false };
+            columnOptions.push(option);
+        });
+        return columnOptions;
+    }
+
+    jTable.DataTable({
+        paging: false,
+        order: [[0, "asc"]],
+        columns: getColumnOptions(),
+    });
+}
+
+export default makeTableSortable;
