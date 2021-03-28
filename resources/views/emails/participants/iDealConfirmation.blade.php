a8a5b8110af5e5be4665d5e5f65690748b2d8328
IrisvM <49585581+IrisvM@users.noreply.github.com>
2021-03-28 16:02:37 +0200

    Update iDealConfirmation.blade.php

--- resources/views/emails/participants/iDealConfirmation.blade.php
+++ resources/views/emails/participants/iDealConfirmation.blade.php
@@ -6,15 +6,15 @@
 	Wij bevestigen hierbij dat de betaling via iDeal door ons is ontvangen en daarmee {{ $participant->volnaam }} definitief staat ingeschreven voor het {{$event->naam}} {{$event->datum_start->format('Y')}} ({{$event->datum_start->format('d-m-Y')}} t/m {{$event->datum_eind->format('d-m-Y')}}) te {{$event->location->plaats}}.
 </p>
 <p>
-	Indien u gebruik maakt van de kortingsregeling, vergeet dan niet om een bewijs van het bruto gezinsinkomen met eventuele andere zaken op te sturen naar<a href="mailto:penningmeester@anderwijs.nl">penningmeester@anderwijs.nl</a> of per post naar Vereniging Anderwijs, Postbus 13228, 3507 LE Utrecht.
+	Indien u gebruik maakt van de kortingsregeling, vergeet dan niet om een bewijs van het bruto gezinsinkomen met eventuele andere zaken op te sturen naar<a href="mailto:penningmeester@anderwijs.nl">penningmeester@anderwijs.nl</a> of per post naar Vereniging Anderwijs, Postbus 13228, 3507 LE Utrecht.a
 </p>
 <p>
-	U bevindt zich nu in <a href="https://www.anderwijs.nl/inschrijven/inschrijven-scholieren/">de tweede stap</a>. van het plaatsingsproces van uw kind voor een Anderwijskamp. Dat wil zeggen dat wij aan de slag gaan om een geschikte leidingploeg samen te te stellen voor dit kamp. U hoeft in deze stap niets te doen. <br/>
+	U bevindt zich nu in de tweede stap van <a href="https://www.anderwijs.nl/inschrijven/inschrijven-scholieren/">het plaatsingsproces</a> van uw kind voor een Anderwijskamp. Dat wil zeggen dat wij aan de slag gaan om een geschikte leidingploeg samen te te stellen voor dit kamp. U hoeft in deze stap niets te doen. <br/>
 	Omdat wij voor alle gevraagde vakken op zoek gaan naar de juiste leiding, hebben we over het algemeen pas ongeveer twee weken voor het kamp de leidingploeg definitief samengesteld. U ontvangt dus uiterlijk twee weken voor het kamp een plaatsingsmail voor uw zoon/dochter, wanneer er genoeg plaats is op het kamp.
 </p>
 
 <p>
-Verder willen we u laten weten dat uw inschrijving onherroepelijk is, zodra wij uw kind geplaatst hebben voor het kamp <a href="https://www.anderwijs.nl/inschrijven/inschrijven-scholieren/">(stap 3 in het proces)</a>. Dit wil zeggen dat u het hele kampbedrag dient te betalen, als u zich na het ontvangen van de plaatsingsmail afmeldt. Wanneer u zich eerder afmeldt, dient u slechts de administratiekosten van 50 euro te betalen.
+	Mocht uw kind om wat voor reden dan ook toch niet op kamp kunnen, dan kunt dat melden bij de kantoorcommissie. Meldt u dit vóór de plaatsing (gewoonlijk 2 weken voor het kamp, wordt per mail bevestigd) dan krijgt u het inschrijfgeld minus 50 euro administratiekosten terug. Bij afmelding na de plaatsing krijgt u geen geld terug. Op onze website vindt u het volledige <a href="https://www.anderwijs.nl/inschrijven/inschrijven-scholieren/" target="_blank">plaatsingsproces</a>.
 </p>
 
 <p>
