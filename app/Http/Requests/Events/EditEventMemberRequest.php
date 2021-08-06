27bda96933348c1bd7e871a2485697d3eee8d67c
Vincent Hagen <vinnie@script4web.nl>
2021-08-06 15:58:05 +0200

    Add form validation for edit event members with form requests.

---
+++ app/Http/Requests/Events/EditEventMemberRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests\Events;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+final class EditEventMemberRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'wissel' => 'required|boolean',
+            'wissel_datum_start' => 'required_if:wissel,1|nullable|date',
+            'wissel_datum_eind' => 'required_if:wissel,1|nullable|date|after:wissel_datum_start',
+        ];
+    }
+
+}
