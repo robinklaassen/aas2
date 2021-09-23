86d92f60b7d7270c1d05fd20c698ad477118060e
Vincent Hagen <vinnie@script4web.nl>
2021-09-23 20:27:52 +0200

    Add test for roles

---
+++ tests/Feature/RolesTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class RolesTest extends TestCase
+{
+    public $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->user = \App\User::findOrFail(1);
+    }
+
+    public function testIndex()
+    {
+        $this
+            ->actingAs($this->user)
+            ->get('/roles/explain')
+            ->assertStatus(200)
+            ->assertSee('Aasbaas', 'Deelnemersinfo - Inzien');
+    }
+
+    public function testShowUnauthorized()
+    {
+        $this
+            ->get('/roles/explain')
+            ->assertStatus(302);
+    }
+
+}
