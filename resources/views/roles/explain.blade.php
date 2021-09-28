37102251a42ca0b3272b8395e2d408450f601281
Vincent Hagen <vinnie@script4web.nl>
2021-09-28 20:09:47 +0200

    Made overview searchable

--- resources/views/roles/explain.blade.php
+++ resources/views/roles/explain.blade.php
@@ -5,29 +5,9 @@ Rollen en Rechten
 @endsection
 
 @section('content')
-<div class="c-roles-explanation">
-    <table class="c-roles-explanation__table">
-        <thead>
-            <tr>
-                <th>
-                    <!-- Capability description -->
-                </th>
-                @foreach($roles as $role)
-                    <th>{{$role->title}}</th>
-                @endforeach
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($capabilities as $capability)
-            <tr>
-                <th>{{ $capability->description }}</th>
-                @foreach($roles as $role)
-                    <td class="c-roles-explanation__table__cell @if($capabilitiesPerRole[$role->id]->contains($capability->id)) --active @endif"></td>
-                @endforeach
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+<roles-table
+    :roles='@json($roles)'
+    :capabilities='@json($capabilities)'
+    :capabilities-per-role='@json($capabilitiesPerRole)'
+></roles-table>
 @endsection
-
