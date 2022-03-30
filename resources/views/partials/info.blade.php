98192156c2025afcf2af7fccf8ae860d020095b1
Vincent Hagen <vinnie@script4web.nl>
2022-03-30 17:05:19 +0200

    April-fools: kaas edition

--- resources/views/partials/info.blade.php
+++ resources/views/partials/info.blade.php
@@ -4,12 +4,12 @@
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-          <h4 class="modal-title">Over AAS 2.0</h4>
+          <h4 class="modal-title">Over KAAS 2.0</h4>
         </div>
         <div class="modal-body">
-          <p>AAS 2.0 is ontwikkeld door <a href="mailto:robin@anderwijs.nl">Robin Klaassen</a> in april-mei 2015. Het systeem draait op het PHP framework <a href="http://laravel.com/">Laravel</a> en de voorkant is gebaseerd op <a href="http://getbootstrap.com/">Bootstrap 3</a>, met de <a href="https://bootswatch.com/cosmo/">Cosmo</a> template. Interactieve tabellen worden mogelijk gemaakt door <a href="https://datatables.net">DataTables</a>, kaarten en grafieken door Google's <a href="https://developers.google.com/maps/">Maps</a> en <a href="https://developers.google.com/chart/">Charts</a>.</p>
+          <p>KAAS 2.0 is ontwikkeld door <a href="mailto:robin@anderwijs.nl">Robin Klaassen</a> in april-mei 2015. Het systeem draait op het PHP framework <a href="http://laravel.com/">Laravel</a> en de voorkant is gebaseerd op <a href="http://getbootstrap.com/">Bootstrap 3</a>, met de <a href="https://bootswatch.com/cosmo/">Cosmo</a> template. Interactieve tabellen worden mogelijk gemaakt door <a href="https://datatables.net">DataTables</a>, kaarten en grafieken door Google's <a href="https://developers.google.com/maps/">Maps</a> en <a href="https://developers.google.com/chart/">Charts</a>.</p>
 
-          <p>AAS staat voor Anderwijs Administratie Systeem en is voor het eerst digitaal ontwikkeld door Dave Liefbroer in 2008.</p>
+          <p>KAAS staat voor Krachtig Anderwijs Administratie Systeem en is voor het eerst digitaal ontwikkeld door Dave Liefbroer in 2008.</p>
 
           <p>Bug reports, feature requests en/of vragen graag per mail naar <a href="mailto:webmaster@anderwijs.nl">webmaster@anderwijs.nl</a>. Wees hierbij zo specifiek mogelijk.</p>
         </div>
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -8,7 +8,7 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="{{ url('/home') }}">AAS 2.0</a>
+					<a class="navbar-brand" href="{{ url('/home') }}">KAAS 2.0</a>
 				</div>
 
 				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
