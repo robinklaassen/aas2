47fffe22383e899356bd91def08e635697f8e6e1
Vincent Hagen <vinnie@script4web.nl>
2022-04-08 16:22:21 +0200

    Removed api namespace prefix and use fqn for controllers

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -50,7 +50,6 @@ class RouteServiceProvider extends ServiceProvider
     protected function mapApiRoutes()
     {
         Route::middleware('api')
-            ->namespace($this->namespace)
             ->group(base_path('routes/api.php'));
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,16 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\ContactFormController;
 use Illuminate\Support\Facades\Route;
 
 // JSON API's for website
-Route::controller('ApiController')->group(function () {
+Route::controller(ApiController::class)->group(function () {
     Route::get('cal/{type}', 'cal');
     Route::get('camp-info/{camp}', 'campInfo');
     Route::get('camps-report', 'campsReport');
 });
 
 // Contact form on website
-Route::post('api/contact-form', 'ContactFormController@send');
+Route::post('api/contact-form', [ContactFormController::class, 'send']);
