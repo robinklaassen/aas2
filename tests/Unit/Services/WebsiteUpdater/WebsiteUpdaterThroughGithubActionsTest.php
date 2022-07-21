03f2c6b716b9bdb8777b25255098cc17e8ffe01c
Vincent Hagen <vinnie@script4web.nl>
2022-07-21 15:56:07 +0200

    Fix CS

---
+++ tests/Unit/Services/WebsiteUpdater/WebsiteUpdaterThroughGithubActionsTest.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Unit\Services\WebsiteUpdater;
+
+use App\Services\WebsiteUpdater\WebsiteUpdaterThroughGithubActions;
+use PHPUnit\Framework\TestCase;
+
+class WebsiteUpdaterThroughGithubActionsTest extends TestCase
+{
+
+}
