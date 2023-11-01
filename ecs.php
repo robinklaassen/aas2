27574a6ac4369eec23e802bdcceac386b7420475
Robin Klaassen <13085028+robinklaassen@users.noreply.github.com>
2023-11-01 14:08:11 +0100

    update ECS to v12 and fix config

--- ecs.php
+++ ecs.php
@@ -3,25 +3,26 @@
 declare(strict_types=1);
 
 use Symplify\EasyCodingStandard\Config\ECSConfig;
-use Symplify\EasyCodingStandard\ValueObject\Option;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
 return static function (ECSConfig $config): void {
-    $config->import(SetList::COMMON);
-    $config->import(SetList::PSR_12);
-    $config->import(SetList::CLEAN_CODE);
+    $config->sets([
+        SetList::COMMON,
+        SetList::PSR_12,
+        SetList::CLEAN_CODE,
+    ]);
 
     // need more fixers? Search them on: https://mlocati.github.io/php-cs-fixer-configurator
-    $services = $config->services();
-    $services->set(PhpCsFixer\Fixer\Comment\SingleLineCommentStyleFixer::class);
-    $services->set(PhpCsFixer\Fixer\ControlStructure\TrailingCommaInMultilineFixer::class);
-    $services->set(PhpCsFixer\Fixer\PhpUnit\PhpUnitMethodCasingFixer::class);
-    $services->set(PhpCsFixer\Fixer\PhpUnit\PhpUnitSetUpTearDownVisibilityFixer::class);
-    $services->set(PhpCsFixer\Fixer\Whitespace\ArrayIndentationFixer::class);
-    $services->set(PhpCsFixer\Fixer\Alias\ArrayPushFixer::class);
+    $config->rules([
+        PhpCsFixer\Fixer\Comment\SingleLineCommentStyleFixer::class,
+        PhpCsFixer\Fixer\ControlStructure\TrailingCommaInMultilineFixer::class,
+        PhpCsFixer\Fixer\PhpUnit\PhpUnitMethodCasingFixer::class,
+        PhpCsFixer\Fixer\PhpUnit\PhpUnitSetUpTearDownVisibilityFixer::class,
+        PhpCsFixer\Fixer\Whitespace\ArrayIndentationFixer::class,
+        PhpCsFixer\Fixer\Alias\ArrayPushFixer::class,
+    ]);
 
-    $parameters = $config->parameters();
-    $parameters->set(Option::PATHS, [
+    $config->paths([
         __DIR__ . '/app/',
         __DIR__ . '/config/',
         __DIR__ . '/database/',
@@ -31,7 +32,7 @@ return static function (ECSConfig $config): void {
         __DIR__ . '/ecs.php',
     ]);
 
-    $parameters->set(Option::SKIP, [
+    $config->skip([
         PhpCsFixer\Fixer\PhpUnit\PhpUnitStrictFixer::class,
     ]);
 };
